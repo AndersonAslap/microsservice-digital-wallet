0791090270a39f6e10a4f5bd18fb7662c7f620ef
AndersonAslap <anderson.adolfo1998@gmail.com>
2023-11-04 17:07:24 -0300

    feat: create controllers

---
+++ microsservices/walletcore/src/infra/http/HttpControllerWalletCore.ts
@@ -0,0 +1,26 @@
+import { UsecaseFactoryInterface } from "../../application/factory/usecase-factory-interface"
+import { HttpServer } from "./HttpServer"
+
+export class HttpControllerWalletCore {
+
+    constructor(httpServer: HttpServer, usecaseFactory: UsecaseFactoryInterface) {
+
+        httpServer.on('post', '/clients', async (params:any, body:any, headers:any) => {
+            const usecase = usecaseFactory.createClientUseCase();
+            const output = await usecase.execute(body);
+            return output
+        });
+
+        httpServer.on('post', '/accounts', async (params:any, body:any, headers:any) => {
+            const usecase = usecaseFactory.createAccountUseCase();
+            const output = await usecase.execute(body);
+            return output
+        });
+
+        httpServer.on('post', '/transaction', async (params:any, body:any, headers:any) => {
+            const usecase = usecaseFactory.createTransactionUseCase();
+            const output = await usecase.execute(body);
+            return output
+        });
+    }
+}
